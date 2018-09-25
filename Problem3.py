e3461c6b8636f0192953a29d78fa40c9a4da74f2
vr266 <vr266@scarletmail.rutgers.edu>
2018-09-24 23:45:38 -0400

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,31 +1,50 @@
-
-class integ:
-    def InsertionSort(l):
-        for i in range(1,len(l)):
-            val = l[i]
-            target = i
-            while target>0 and l[target-1]>val:
-            l[target]=l[target-1]
-            target = target-1
-         l[target]=val
-    def Push(q,l):
-        l+=[0]*1
-        n=len(l)
-        i=n
+class ArbitraryArray: 
+    l=[]
+    count=1
+    def ArbitraryArray(self):
+          z=[]  
+    def Insert(self,e,q):
+        n=len(ArbitraryArray.l)
+        if e>n:
+            print("change") 
+        else:
+            ArbitraryArray.l +=[0]*1
+            i=len(ArbitraryArray.l)
+            while i>e:
+                ArbitraryArray.l[i-1]=ArbitraryArray.l[i-2]
+                i-=1
+            ArbitraryArray.l[e]=q
+            print(ArbitraryArray.l)
+    def Push(self,q):
+            ArbitraryArray.l += [0]*1
+            
+            i=len(ArbitraryArray.l)
+            while i>0:
+                ArbitraryArray.l[i-1]=ArbitraryArray.l[i-2]
+                i-=1
+            ArbitraryArray.l[0]=q
+            print(ArbitraryArray.l)
+            ArbitraryArray.count+=1
+    def Enqueue(self,q):
+        ArbitraryArray.l += [0]*1
+        ArbitraryArray.l[len(ArbitraryArray.l)-1]=q
+        print(ArbitraryArray.l)
+    def Pop(self):
+        z=[0]*(len(ArbitraryArray.l)-1)
+        i=len(z)
         while i>0:
-            l[i-1]=l[i-2]
-            i-=1
-        l[0]=q
-        print(l)
-    def Pop(l):
-        w=len(l)-1
-        z=list(range(w))
+            z[i-1]=ArbitraryArray.l[i]
+            i-=1  
+        print(ArbitraryArray.l[0])
         print(z)
+    def Dequeue(self):
+        z=[0]*(len(ArbitraryArray.l)-2)
         i=0
-    while i<len(l)-1:
-        z[i]=l[i+1]
-        i+=1
-    print(z)
-    print(l[0])
-    def PastPeek(l):
-        print(l[len(l)-1])
+        print(z)
+        while i<len(z):
+            z[i]=ArbitraryArray.l[i+1]
+            i+=1  
+        print(ArbitraryArray.l[len(z)+1])
+        print(z)
+    def Transverse(self,q)
+        print(ArbitraryArray.l[q])
